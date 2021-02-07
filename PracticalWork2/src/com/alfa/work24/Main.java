196d18c48640c072a3b343ee198a6a2a0def6fb5
aleksandrprimak <78693885+aleksandrprimak@users.noreply.github.com>
2021-02-07 18:28:00 +0200

    Add files via upload

---
+++ PracticalWork2/src/com/alfa/work24/Main.java
@@ -0,0 +1,31 @@
+package com.alfa.work24;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+       System.out.println("-----------------------" );
+       System.out.print("Imput: " );
+       Scanner scanner = new Scanner(System.in);
+       int q = scanner.nextInt();
+       String w = String.valueOf(q);
+
+       System.out.println("-----------------------" );
+       int number,reverse_number,save_number;
+       number = q;
+       save_number=number;
+       reverse_number = 0;
+       System.out.println("oring: " + number );
+       reverse_number = reverse_number*10 + number%10;
+       number = number/10;
+       reverse_number = reverse_number*10 + number%10;
+       number = number/10;
+       reverse_number = reverse_number*10 + number%10;
+       number = number/10;
+       System.out.println("rewers: " + reverse_number);
+       System.out.println("difference: " + (q - reverse_number));
+    }
+}
